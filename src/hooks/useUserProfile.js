9240950eca5275635581cd2f45b1fa9675fb3e8d
Carlos Lopes <dev.carloslopes@gmail.com>
2022-01-15 15:43:10 -0300

    feat: contexto de usuario para controle de estado global

---
+++ src/hooks/useUserProfile.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/jsx-no-constructed-context-values */
+import { createContext, useContext, useState } from 'react';
+
+const UserContext = createContext({});
+const useUserProfile = () => useContext(UserContext);
+
+function UserProvider(props) {
+  const [user, setUser] = useState(null);
+
+  return <UserContext.Provider value={{ user, setUser }} {...props} />;
+}
+
+export { UserProvider, useUserProfile };
