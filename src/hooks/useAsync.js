3382409f2be4fd1d202d8f43eed45c2ec99d35d8
Carlos Lopes <dev.carloslopes@gmail.com>
2022-01-15 15:39:44 -0300

    feat: useAsync para trabalhar melhor com as requisiçoes

---
+++ src/hooks/useAsync.js
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useState } from 'react';
+
+function useAsync(asyncFunction, immediate = true) {
+  const [status, setStatus] = useState('idle');
+  const [value, setValue] = useState(null);
+  const [error, setError] = useState(null);
+
+  // Executa a asyncFunction vinda dos parametros
+  // useCallback usado para não causar rerenders
+  // e executar quando asyncFunction for chamada
+  const execute = useCallback(() => {
+    setStatus('pending');
+    setValue(null);
+    setError(null);
+
+    return asyncFunction()
+      .then((response) => {
+        setValue(response);
+        setStatus('success');
+      })
+      .catch((err) => {
+        setError(err);
+        setStatus('error');
+      });
+  }, [asyncFunction]);
+
+  // Se falso executa apenas quando execute for chamado
+  useEffect(() => {
+    if (immediate) {
+      execute();
+    }
+  }, [execute, immediate]);
+
+  return {
+    execute,
+    status,
+    value,
+    error,
+  };
+}
+
+export default useAsync;
